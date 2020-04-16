8453cbb795534a1068b8029789b3a052d76d74d2
LaceyStein <lacey.stein@stonybrook.edu>
2020-04-15 20:07:51 -0400

    Can get state name from JSON

--- src/DatabaseSimulator.java
+++ src/DatabaseSimulator.java
@@ -3,6 +3,7 @@ import org.json.simple.parser.*;
 
 import java.io.*;
 import java.util.Iterator;
+import java.util.Set;
 
 public class DatabaseSimulator {
 
@@ -15,10 +16,20 @@ public class DatabaseSimulator {
             Reader reader = new FileReader("/Users/lacey/IdeaProjects/Eagles416FinalProject2/src/us-states.json");
             JSONObject jsonObject = (JSONObject) parser.parse(reader);
 
-            //JSONObject jsonObject = (JSONObject) parser.parse(new InputStreamReader(new FileInputStream("us-states.json")));
-            String name = jsonObject.toString();
-            System.out.println(name);
-            stateName = name;
+            Set key = jsonObject.keySet();
+
+            JSONArray array = (JSONArray) jsonObject.get(key.iterator().next());
+
+            System.out.println("The 1st element of array");
+            System.out.println(array.get(0));
+            System.out.println();
+
+            JSONObject obj = (JSONObject)array.get(0);
+            JSONObject properties = (JSONObject) obj.get("properties");
+            System.out.println(properties.get("name"));
+
+
+            stateName = properties.get("name").toString();
         }catch(Exception e) {
             System.out.println("\n\n\n\n\n" + e.toString());
         }
