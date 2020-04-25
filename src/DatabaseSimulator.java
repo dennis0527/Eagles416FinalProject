2c0e81eab41299f1acf1004f33284a0ff18bc809
Dennis Heerlein <dennis.heerlein@stonybrook.edu>
2020-04-25 19:33:35 -0400

    Florida neighbors added

--- src/DatabaseSimulator.java
+++ src/DatabaseSimulator.java
@@ -11,7 +11,7 @@ public class DatabaseSimulator {
         JSONParser parser = new JSONParser();
 
         try {
-            Reader reader = new FileReader("/Users/lacey/IdeaProjects/Eagles416FinalProject2/src/us-states.json");
+            Reader reader = new FileReader("/Users/dennisheerlein/IdeaProjects/election_data_quality_eagles/src/us-states.json");
             JSONObject jsonObject = (JSONObject) parser.parse(reader);
             String result = "";
 
@@ -36,15 +36,15 @@ public class DatabaseSimulator {
     public static String getPrecinctJson(String stateName) {
         String fileName = "";
         if (stateName.equals("Maryland")) {
-            fileName = "/Users/lacey/IdeaProjects/Eagles416FinalProject2/src/MD_data_raw.json";
+            fileName = "/Users/dennisheerlein/IdeaProjects/election_data_quality_eagles/src/MD_data_raw.json";
         } else if (stateName.equals("Florida")) {
-            fileName = "/Users/lacey/IdeaProjects/Eagles416FinalProject2/src/FL_demographicscopy.json";
+            fileName = "/Users/dennisheerlein/IdeaProjects/election_data_quality_eagles/src/FL_demographics.json";
         } else if (stateName.equals("FloridaElection")) {
-            fileName = "/Users/lacey/IdeaProjects/Eagles416FinalProject2/src/FL_precinctcopy.json";
+            fileName = "/Users/dennisheerlein/IdeaProjects/election_data_quality_eagles/src/FL_precinct.json";
         } else if (stateName.equals("New York")) {
-            fileName = "/Users/lacey/IdeaProjects/Eagles416FinalProject2/src/NY_demographics.json";
+            fileName = "/Users/dennisheerlein/IdeaProjects/election_data_quality_eagles/src/NY_demographics.json";
         } else if(stateName.equals("New York Election")) {
-            fileName = "/Users/lacey/IdeaProjects/Eagles416FinalProject2/src/NY_precinct_2.json";
+            fileName = "/Users/dennisheerlein/IdeaProjects/election_data_quality_eagles/src/NY_precinct_2.json";
         }
 
 
@@ -69,7 +69,7 @@ public class DatabaseSimulator {
         JSONParser parser = new JSONParser();
 
         try {
-            Reader reader = new FileReader("/Users/lacey/IdeaProjects/Eagles416FinalProject2/src/congressional_json.json");
+            Reader reader = new FileReader("/Users/dennisheerlein/IdeaProjects/election_data_quality_eagles/src/congressional_json.json");
             JSONObject jsonObject = (JSONObject) parser.parse(reader);
 
             return jsonObject.toString();
@@ -89,13 +89,13 @@ public class DatabaseSimulator {
         try {
             BufferedReader reader = null;
             if (stateName.equals("Maryland")) {
-                reader = new BufferedReader(new FileReader("/Users/lacey/IdeaProjects/Eagles416FinalProject2/src/MD_neighbors.txt"));
+                reader = new BufferedReader(new FileReader("/Users/dennisheerlein/IdeaProjects/election_data_quality_eagles/src/MD_neighbors.txt"));
             }
             else if (stateName.equals("Florida")) {
-                reader = new BufferedReader(new FileReader("/Users/lacey/IdeaProjects/Eagles416FinalProject2/src/FL_neighbors.json"));
+                reader = new BufferedReader(new FileReader("/Users/dennisheerlein/IdeaProjects/election_data_quality_eagles/src/FL_neighbors.json"));
             }
             else if (stateName.equals("New York")) {
-                reader = new BufferedReader(new FileReader("/Users/lacey/IdeaProjects/Eagles416FinalProject2/src/NY_neighbors.json"));
+                reader = new BufferedReader(new FileReader("/Users/dennisheerlein/IdeaProjects/election_data_quality_eagles/src/NY_neighbors.json"));
             }
             String line = reader.readLine();
 
@@ -126,7 +126,7 @@ public class DatabaseSimulator {
         JSONParser parser = new JSONParser();
         if (stateName.equals("Maryland")) {
             try {
-                Reader reader = new FileReader("/Users/lacey/IdeaProjects/Eagles416FinalProject2/src/MD_anomalous.json");
+                Reader reader = new FileReader("/Users/dennisheerlein/IdeaProjects/election_data_quality_eagles/src/MD_anomalous.json");
                 JSONObject jsonObject = (JSONObject) parser.parse(reader);
 
                 return jsonObject.toString();
@@ -146,7 +146,7 @@ public class DatabaseSimulator {
     JSONParser parser = new JSONParser();
 
     try {
-        BufferedReader reader = new BufferedReader(new FileReader("/Users/lacey/IdeaProjects/Eagles416FinalProject2/src/FL_national_park.json"));
+        BufferedReader reader = new BufferedReader(new FileReader("/Users/dennisheerlein/IdeaProjects/election_data_quality_eagles/src/FL_national_park.json"));
         JSONObject object = (JSONObject) parser.parse(reader);
         return object.toString();
 
